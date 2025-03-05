72c332a523c3a0446615492931433e899f84fe0b
linoyniazov <linoy2209@gmail.com>
2025-03-05 14:14:34 +0200

    Added BottomNavigationView

--- app/src/main/java/com/example/styleshare/MainActivity.kt
+++ app/src/main/java/com/example/styleshare/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.styleshare.R
+import androidx.navigation.ui.NavigationUI
+import com.example.styleshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
